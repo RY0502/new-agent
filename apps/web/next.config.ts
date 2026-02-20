2c91fe52852702f48741ac4f0daa36ec4429db6d
Ravi Yadav <ravi_yadav0502@yahoo.in>
2026-02-20 15:35:21 +0530

    change

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@copilotkit/runtime"],
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
